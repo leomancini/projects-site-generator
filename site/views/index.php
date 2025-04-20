b9f01adae24af80db3a4b0db456bb45b6561552c
Leo Mancini <mancini@leo.gd>
2025-04-20 19:15:10 -0400

    Updating paths

--- site/views/index.php
+++ site/views/index.php
@@ -7,11 +7,11 @@
 <html>
 	<head>
 		<title>Leo Mancini &ndash; Projects</title>
-		<link rel="icon" type="image/png" sizes="32x32" href="resources/images/favicon-32.png">
-		<link rel="icon" type="image/png" sizes="16x16" href="resources/images/favicon-16.png">
-		<link rel="apple-touch-icon" sizes="180x180" href="resources/images/apple-touch-icon.png">
-		<link rel="mask-icon" href="resources/images/safari-pinned-tab.svg" color="#000000">
-		<link rel="manifest" href="/manifest.json">
+		<link rel="icon" type="image/png" sizes="32x32" href="site/resources/images/favicon-32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="site/resources/images/favicon-16.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="site/resources/images/apple-touch-icon.png">
+		<link rel="mask-icon" href="site/resources/images/safari-pinned-tab.svg" color="#000000">
+		<link rel="manifest" href="site/manifest.json">
 		<link rel='stylesheet/less' href='site/resources/css/index.less<?php if ($config['debug'] === true) { echo '?hash='.rand(0, 9999); } ?>'>
 		<script src='site/resources/js/lib/less.js'></script>
 		<script src='site/resources/js/lib/jquery.js'></script>
