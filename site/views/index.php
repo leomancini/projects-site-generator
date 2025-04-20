3bcae6bf7174ba3114dc8e304a0ef5e216e3171c
Leo Mancini <mancini@leo.gd>
2025-04-20 19:18:34 -0400

    Updating icons for light and dark mode

--- site/views/index.php
+++ site/views/index.php
@@ -7,8 +7,10 @@
 <html>
 	<head>
 		<title>Leo Mancini &ndash; Projects</title>
-		<link rel="icon" type="image/png" sizes="32x32" href="site/resources/images/favicon-32.png">
-		<link rel="icon" type="image/png" sizes="16x16" href="site/resources/images/favicon-16.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="site/resources/images/favicon-32-light.png" media="(prefers-color-scheme: light)">
+		<link rel="icon" type="image/png" sizes="32x32" href="site/resources/images/favicon-32-dark.png" media="(prefers-color-scheme: dark)">
+		<link rel="icon" type="image/png" sizes="16x16" href="site/resources/images/favicon-16-light.png" media="(prefers-color-scheme: light)">
+		<link rel="icon" type="image/png" sizes="16x16" href="site/resources/images/favicon-16-dark.png" media="(prefers-color-scheme: dark)">
 		<link rel="apple-touch-icon" sizes="180x180" href="site/resources/images/apple-touch-icon.png">
 		<link rel="mask-icon" href="site/resources/images/safari-pinned-tab.svg" color="#000000">
 		<link rel="manifest" href="site/manifest.json">
