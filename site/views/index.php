f2b600c89d9d7c2c669b5ef1e77f11ba4b922d45
Leo Mancini <mancini@leo.gd>
2025-04-21 22:49:18 -0400

    Updating icon version

--- site/views/index.php
+++ site/views/index.php
@@ -1,18 +1,19 @@
 <?php
-    include('../controllers/base.php');
+  include('../controllers/base.php');
 
 	$config = loadConfig();
+	$iconVersion = '04222025';
 ?>
 <!DOCTYPE HTML>
 <html>
 	<head>
 		<title>Leo Mancini &ndash; Projects</title>
-		<link rel="icon" type="image/png" sizes="32x32" href="site/resources/images/favicon-32-light.png" media="(prefers-color-scheme: light)">
-		<link rel="icon" type="image/png" sizes="32x32" href="site/resources/images/favicon-32-dark.png" media="(prefers-color-scheme: dark)">
-		<link rel="icon" type="image/png" sizes="16x16" href="site/resources/images/favicon-16-light.png" media="(prefers-color-scheme: light)">
-		<link rel="icon" type="image/png" sizes="16x16" href="site/resources/images/favicon-16-dark.png" media="(prefers-color-scheme: dark)">
-		<link rel="apple-touch-icon" sizes="180x180" href="site/resources/images/apple-touch-icon.png?v=04212025">
-		<link rel="mask-icon" href="site/resources/images/safari-pinned-tab.svg" color="#000000">
+		<link rel="icon" type="image/png" sizes="32x32" href="site/resources/images/favicon-32-light.png?v=<?php echo $iconVersion; ?>" media="(prefers-color-scheme: light)">
+		<link rel="icon" type="image/png" sizes="32x32" href="site/resources/images/favicon-32-dark.png?v=<?php echo $iconVersion; ?>" media="(prefers-color-scheme: dark)">
+		<link rel="icon" type="image/png" sizes="16x16" href="site/resources/images/favicon-16-light.png?v=<?php echo $iconVersion; ?>" media="(prefers-color-scheme: light)">
+		<link rel="icon" type="image/png" sizes="16x16" href="site/resources/images/favicon-16-dark.png?v=<?php echo $iconVersion; ?>" media="(prefers-color-scheme: dark)">
+		<link rel="apple-touch-icon" sizes="180x180" href="site/resources/images/apple-touch-icon.png?v=<?php echo $iconVersion; ?>">
+		<link rel="mask-icon" href="site/resources/images/safari-pinned-tab.svg?v=<?php echo $iconVersion; ?>" color="#000000">
 		<link rel="manifest" href="site/manifest.json">
 		<link rel='stylesheet/less' href='site/resources/css/index.less<?php if ($config['debug'] === true) { echo '?hash='.rand(0, 9999); } ?>'>
 		<script src='site/resources/js/lib/less.js'></script>
